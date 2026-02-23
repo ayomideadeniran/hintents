4fba491bc11bd97fbfc42b769ec0035fa1f0661a
Ikem Peter <ikempeter2020@gmail.com>
2026-02-24 00:45:32 +0100

    fix(cli): add structured interrupt handling and shutdown coordinator

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/dotandev/hintents/internal/cmd"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
+	os.Exit(run(cmd.Execute, os.Stderr))
+}
+
+func run(execute func() error, stderr io.Writer) int {
+	if err := execute(); err != nil {
+		if cmd.IsInterrupted(err) {
+			fmt.Fprintln(stderr, "Interrupted. Shutting down...")
+			return cmd.InterruptExitCode
+		}
+		fmt.Fprintf(stderr, "Error: %v\n", err)
+		return 1
 	}
+	return 0
 }
